7218fc6f7b3b034163c805bf763f6378ad101170
IlidanNaga <max.korn@bk.ru>
2019-09-23 12:49:05 +0300

    23.09.19

--- my_attempt.py
+++ my_attempt.py
@@ -1,5 +1,6 @@
 import pygame as pyg
 
+from my_ball import Ball
 
 """Main class, used to run mainloop of game"""
 
@@ -15,19 +16,36 @@ class Game(object):
         self.width = infoObject.current_w
         self.height = infoObject.current_h
 
+        self.animation_width = round(self.width * 0.6)
+        self.animation_height = round(self.height * 0.6)
+
+        self.animation_screen = pyg.display.set_mode((self.animation_width, self.animation_height))
+
         #self.screen = pyg.display.set_mode((self.width, self.height))
         self.background = pyg.Surface(self.screen.get_size()).convert()
         self.background.fill((255, 255, 255))
 
         self.act_surface = self.screen
+        self.act_width = self.width
+        self.act_height = self.height
 
     def draw_static(self):
 
         self.act_surface = self.background
+        self.act_width = self.width
+        self.act_height = self.height
 
     def draw_dynamic(self):
 
         self.act_surface = self.screen
+        self.act_width = self.width
+        self.act_height = self.height
+
+    def draw_animation(self):
+
+        self.act_surface = self.animation_screen
+        self.act_width = self.animation_width
+        self.act_height = self.animation_height
 
     def circle(self, x, y, radius, width, color):
 
@@ -58,65 +76,6 @@ class Game(object):
 
         pyg.quit()
 
-"""Ball class, object we test"""
-
-class Ball(object):
-
-    def __init__(self, x, y, radius, speed_x=1, speed_y=0, speed_pulse=0, color=(127, 127, 127), width = 0):
-
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.act_radius = radius
-        self.speed_x = speed_x
-        self.speed_y = speed_y
-        self.speed_pulse = speed_pulse #radius change speed
-        self.color = color
-        self.width = width
-        self.shrinking = True
-
-    def max_x(self):
-
-        return self.x + self.radius
-
-    def min_x(self):
-
-        return self.x - self.radius
-
-    def max_y(self):
-
-        return self.y + self.radius
-
-    def min_y(self):
-
-        return self.y - self.radius
-
-    def rel_move(self):
-
-        self.x += self.speed_x
-        self.y += self.speed_y
-
-    def change_speed_x(self, new):
-
-        self.speed_x = new
-
-    def change_speed_y(self, new):
-
-        self.speed_y = new
-
-    def check_bump(self, width, height):
-
-        if (self.max_x() + self.speed_x > width) or (self.min_x() + self.speed_x < 0):
-            self.speed_x *= -1
-
-        if self.max_y() + self.speed_y > height or self.min_y() + self.speed_y < 0:
-            self.speed_y *= -1
-
-    def draw(self, view):
-
-        view.circle(self.x, self.y, self.act_radius, self.width, self.color)
-
-
 """action function"""
 
 def action(balls, width, height, view):
@@ -137,10 +96,10 @@ def main():
 
     view.draw_dynamic()
 
-    ball1 = Ball(1000, 100, 10, 10, 2, 0, (127, 127, 127))
+    ball1 = Ball(100, 100, 10, 10, 2, 0, (127, 127, 127))
     ball2 = Ball(100, 400, 10, 2, -2, 0, (0, 255, 0))
 
-    loopfunc = action((ball1, ball2), view.width, view.height, view)
+    loopfunc = action((ball1, ball2), view.act_width, view.act_height, view)
     view.run(loopfunc)
 
 
